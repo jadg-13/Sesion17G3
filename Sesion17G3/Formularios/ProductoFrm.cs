b24a8b338be2185572eddbb8bc240bc3c1a9b350
jadg13 <86695996+jadg-13@users.noreply.github.com>
2024-10-14 08:09:17 -0600

    Se agrego la clase ProductoServicio y la clase ProductoFrm

---
+++ Sesion17G3/Formularios/ProductoFrm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sesion17G3.Formularios
+{
+    public partial class ProductoFrm : Form
+    {
+        public ProductoFrm()
+        {
+            InitializeComponent();
+        }
+    }
+}
